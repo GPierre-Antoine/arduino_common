73a79c7000804ab7e2fbef0d860f96a88bba6edd
Pierre-Antoine <pierreantoine.guillaume@gmail.com>
2017-01-18 12:58:32 +0100

    array ; long long => long, const_cast removed and {0} => {T()}

--- array.h
+++ array.h
@@ -7,21 +7,21 @@
 
 namespace pag
 {
-    template <typename T, unsigned long long int D>
+    template <typename T, unsigned long int D>
     class array
     {
     public:
 
         //current array size
-        unsigned long long int size() const;
+        unsigned long int size() const;
 
         //ref accessor
-        T & operator[](const unsigned long long int index);
+        T & operator[](const unsigned long int index);
         //const ref accessor
-        const T &  operator[](const unsigned long long int index) const;
+        const T &  operator[](const unsigned long int index) const;
 
-        T& reverse(const unsigned long long int index);
-        const T &  reverse(const unsigned long long int index) const;
+        T& reverse(const unsigned long int index);
+        const T &  reverse(const unsigned long int index) const;
 
         //constructor, with c style array or nothing
         array(T * underlaying);
@@ -35,51 +35,51 @@ namespace pag
         const T * end() const;
 
     private:
-        T  p[D] = {0};//actual container
+        T  p[D] = {T()};//actual container
     };
 }
 
-template <typename T, unsigned long long int D>
-unsigned long long int pag::array<T,D>::size() const
+template <typename T, unsigned long int D>
+unsigned long int pag::array<T,D>::size() const
 {
     return D;
 }
 
 
 
-template <typename T, unsigned long long int D>
-const T & pag::array<T,D>::operator[](const unsigned long long int index) const
+template <typename T, unsigned long int D>
+const T & pag::array<T,D>::operator[](const unsigned long int index) const
 {
     return p[index];
 }
 
-template <typename T, unsigned long long int D>
-T & pag::array<T,D>::operator[](const unsigned long long int index)
+template <typename T, unsigned long int D>
+T & pag::array<T,D>::operator[](const unsigned long int index)
 {
-    return const_cast<T&>(static_cast<const T&>(this->operator[](index)));
+    return p[index];
 }
 
-template <typename T, unsigned long long int D>
-const T & pag::array<T,D>::reverse(const unsigned long long int index) const
+template <typename T, unsigned long int D>
+const T & pag::array<T,D>::reverse(const unsigned long int index) const
 {
     return p[size()-index];
 }
 
-template <typename T, unsigned long long int D>
-T & pag::array<T,D>::reverse(const unsigned long long int index)
+template <typename T, unsigned long int D>
+T & pag::array<T,D>::reverse(const unsigned long int index)
 {
-    return const_cast<T&>(static_cast<const T&>(this->reverse(index)));
+    return p[size()-index];
 }
 
 
 
-template <typename T, unsigned long long int D>
+template <typename T, unsigned long int D>
 pag::array<T,D>::array()
 {
 
 }
 
-template <typename T, unsigned long long int D>
+template <typename T, unsigned long int D>
 pag::array<T,D>::array(T * array)
 {
     for (unsigned i =0;i<D;++i)
@@ -88,31 +88,31 @@ pag::array<T,D>::array(T * array)
     }
 }
 
-template <typename T, unsigned long long int D>
+template <typename T, unsigned long int D>
 pag::array<T,D>::~array()
 {
 }
 
 
-template <typename T, unsigned long long int D>
+template <typename T, unsigned long int D>
 const T * pag::array<T,D>::begin() const
 {
     return p;
 }
 
-template <typename T, unsigned long long int D>
+template <typename T, unsigned long int D>
 const T * pag::array<T,D>::end() const
 {
-    return p + size();
+    return p+size();
 }
 
-template <typename T, unsigned long long int D>
+template <typename T, unsigned long int D>
 T * pag::array<T,D>::begin()
 {
     return p;
 }
 
-template <typename T, unsigned long long int D>
+template <typename T, unsigned long int D>
 T * pag::array<T,D>::end()
 {
     return p+size();
